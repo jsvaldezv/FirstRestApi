9254878eb30928b925ab8924d8537927e69326f8
Jesús Valdez <jsvaldezv@yahoo.com>
2022-02-01 19:28:52 -0600

    Metodo POST working

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -12,8 +12,23 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', jsonParser, (req, res) => {
-    console.log(req.body);
+    const {title, director, year, rating} = req.body;
+    if(title && director && year && rating)
+    {
+        const id = movies.length + 1;
+        const newMovie = {...req.body, id};
+        movies.push(newMovie)
+        res.json(movies);
+    }
+    else{
+        res.status(500).json({error: 'There was and error'});
+    }
+
     res.send("recieved");
+});
+
+router.delete('/:id', (req, res) => {
+    
 });
 
 module.exports = router;
