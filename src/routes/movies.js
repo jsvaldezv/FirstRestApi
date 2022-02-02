7f101c566b9fe07c088097c45f0ec76dad4f8a7a
Jesús Valdez <jsvaldezv@yahoo.com>
2022-02-01 19:01:41 -0600

    Fix problem with parse json

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,13 +1,17 @@
 const { Router } = require('express');
+var bodyParser = require('body-parser')
 const router = Router();
 
 const movies = require('../sample.json');
+
+// create application/json parser
+var jsonParser = bodyParser.json()
 
 router.get('/', (req, res) => {
     res.json(movies);
 });
 
-router.post('/', (req, res) => {
+router.post('/', jsonParser, (req, res) => {
     console.log(req.body);
     res.send("recieved");
 });
